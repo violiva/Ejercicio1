9f288aab0462cb54f2407946e6b108a08cea0201
Vicente Oliva <vicente_Oliva@hotmail.com>
2015-01-08 11:34:17 +0100

    Initial Commit

---
+++ Ejercicio1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Ejercicio1
+//
+//  Created by Vicente Oliva de la Serna on 8/1/15.
+//  Copyright (c) 2015 Vicente Oliva de la Serna. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
